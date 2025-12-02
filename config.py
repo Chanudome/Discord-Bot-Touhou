faf13ab6f65d2859edb11a669ee1da533b75284e
Chanudome Ngoenanant <111429306+Chanudome@users.noreply.github.com>
2025-12-02 13:53:37 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,17 +3,17 @@
 RSS_SOURCES = [
     {
         "name": "Touhou Yomoyama (Official News)", 
-        "url": "https://touhou-project.news/feed", 
+        "url": "https://touhou-project.news/news/", 
         "type": "official"
     },
     {
         "name": "Touhou Garakuta (Official Magazine)", 
-        "url": "https://touhougarakuta.com/feed", 
+        "url": "https://touhougarakuta.com/news", 
         "type": "magazine"
     },
     {
         "name": "Reddit r/touhou (Community)", 
-        "url": "https://www.reddit.com/r/touhou/new/.rss", 
+        "url": "https://www.reddit.com/r/touhou/new/", 
         "type": "community"
     }
 ]
